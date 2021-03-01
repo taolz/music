1a3865c6c50490f74e9205101052c29906ac6257
taolz <taolz@outlook.com>
2021-03-01 17:40:03 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  lintOnSave: false,
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(html)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'html-loader',
+            options: {
+              minimize: true
+            }
+          }
+        }
+      ]
+    }
+  }
+}
